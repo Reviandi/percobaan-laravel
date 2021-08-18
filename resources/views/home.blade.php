618f3b3bd3eab87c8385ae1354867df02afe0e10
Reviandi <fahmikecil16@gmail.com>
2021-08-18 08:42:32 +0700

    mengganti

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,8 @@
   @extends('layouts.main')
 
   @section('container')
+
       <h1>Halaman Imah</h1>
       <br>
-      <h3 class="hv">Selamat Datang <a href="#" style="text-decoration:none; color:red;">Reviandi</a> & <a href="#" style="text-decoration: none; color:red;">Adi 3 N</a></h3>
+      <h3 class="hv">Selamat Datang <a href="#" style="text-decoration:none; color:red;">Reviandi</a></h3>
   @endsection
